204444ef203dc077d1eecf1ab7a1e3381295d8a1
phlshenrique <phls73376@gmail.com>
2022-12-20 09:03:24 -0300

    Initial commit

---
+++ src/main/java/com/cycloid/epg/EpgApplication.java
@@ -0,0 +1,13 @@
+package com.cycloid.epg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpgApplication.class, args);
+	}
+
+}
